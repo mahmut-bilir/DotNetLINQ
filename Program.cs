2455545e9e01a9b15043ada450da8bc525212994
Mahmut Bilir <mahmut-bilir@hotmail.com>
2025-03-18 17:27:46 +0300

    Where

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+var evenNumbers = numbers.Where(s => s % 2 == 0);
+
+foreach (var evenNumber in evenNumbers)
+{
+    Console.WriteLine(evenNumber);
+}
